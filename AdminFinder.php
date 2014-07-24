f2a2641e97c26ebea1cc63bca4f317db525535a9
Ralph Corleone <r4lph_is_here@live.com>
2014-07-24 10:11:50 -0500

    Create AdminFinder.php

---
+++ AdminFinder.php
@@ -0,0 +1,860 @@
+<?php set_time_limit(0);
+error_reporting(0);
+$list['front'] = "admin
+adm
+admincp
+admcp
+cp
+modcp
+moderatorcp
+adminare
+admins
+cpanel
+controlpanel";
+$list['end'] = "admin1.php
+admin1.html
+admin2.php
+admin2.html
+yonetim.php
+yonetim.html
+yonetici.php
+yonetici.html
+ccms/
+ccms/login.php
+ccms/index.php
+maintenance/
+webmaster/
+adm/
+configuration/
+configure/
+websvn/
+admin/
+admin/account.php
+admin/account.html
+admin/index.php
+admin/index.html
+admin/login.php
+admin/login.html
+admin/home.php
+admin/controlpanel.html
+admin/controlpanel.php
+admin.php
+admin.html
+admin/cp.php
+admin/cp.html
+cp.php
+cp.html
+administrator/
+administrator/index.html
+administrator/index.php
+administrator/login.html
+administrator/login.php
+administrator/account.html
+administrator/account.php
+administrator.php
+administrator.html
+login.php
+login.html
+modelsearch/login.php
+moderator.php
+moderator.html
+moderator/login.php
+moderator/login.html
+moderator/admin.php
+moderator/admin.html
+moderator/
+account.php
+account.html
+controlpanel/
+controlpanel.php
+controlpanel.html
+admincontrol.php
+admincontrol.html
+adminpanel.php
+adminpanel.html
+admin1.asp
+admin2.asp
+yonetim.asp
+yonetici.asp
+admin/account.asp
+admin/index.asp
+admin/login.asp
+admin/home.asp
+admin/controlpanel.asp
+admin.asp
+admin/cp.asp
+cp.asp
+administrator/index.asp
+administrator/login.asp
+administrator/account.asp
+administrator.asp
+login.asp
+modelsearch/login.asp
+moderator.asp
+moderator/login.asp
+moderator/admin.asp
+account.asp
+controlpanel.asp
+admincontrol.asp
+adminpanel.asp
+fileadmin/
+fileadmin.php
+fileadmin.asp
+fileadmin.html
+administration/
+administration.php
+administration.html
+sysadmin.php
+sysadmin.html
+phpmyadmin/
+myadmin/
+sysadmin.asp
+sysadmin/
+ur-admin.asp
+ur-admin.php
+ur-admin.html
+ur-admin/
+Server.php
+Server.html
+Server.asp
+Server/
+wp-admin/
+administr8.php
+administr8.html
+administr8/
+administr8.asp
+webadmin/
+webadmin.php
+webadmin.asp
+webadmin.html
+administratie/
+admins/
+admins.php
+admins.asp
+admins.html
+administrivia/
+Database_Administration/
+WebAdmin/
+useradmin/
+sysadmins/
+admin1/
+system-administration/
+administrators/
+pgadmin/
+directadmin/
+staradmin/
+ServerAdministrator/
+SysAdmin/
+administer/
+LiveUser_Admin/
+sys-admin/
+typo3/
+panel/
+cpanel/
+cPanel/
+cpanel_file/
+platz_login/
+rcLogin/
+blogindex/
+formslogin/
+autologin/
+support_login/
+meta_login/
+manuallogin/
+simpleLogin/
+loginflat/
+utility_login/
+showlogin/
+memlogin/
+members/
+login-redirect/
+sub-login/
+wp-login/
+login1/
+dir-login/
+login_db/
+xlogin/
+smblogin/
+customer_login/
+UserLogin/
+login-us/
+acct_login/
+admin_area/
+bigadmin/
+project-admins/
+phppgadmin/
+pureadmin/
+sql-admin/
+radmind/
+openvpnadmin/
+wizmysqladmin/
+vadmind/
+ezsqliteadmin/
+hpwebjetadmin/
+newsadmin/
+adminpro/
+Lotus_Domino_Admin/
+bbadmin/
+vmailadmin/
+Indy_admin/
+ccp14admin/
+irc-macadmin/
+banneradmin/
+sshadmin/
+phpldapadmin/
+macadmin/
+administratoraccounts/
+admin4_account/
+admin4_colon/
+radmind-1/
+Super-Admin/
+AdminTools/
+cmsadmin/
+SysAdmin2/
+globes_admin/
+cadmins/
+phpSQLiteAdmin/
+navSiteAdmin/
+server_admin_small/
+logo_sysadmin/
+server/
+database_administration/
+power_user/
+system_administration/
+ss_vms_admin_sm/
+adminarea/
+bb-admin/
+adminLogin/
+panel-administracion/
+instadmin/
+memberadmin/
+administratorlogin/
+admin/admin.php
+admin_area/admin.php
+admin_area/login.php
+siteadmin/login.php
+siteadmin/index.php
+siteadmin/login.html
+admin/admin.html
+admin_area/index.php
+bb-admin/index.php
+bb-admin/login.php
+bb-admin/admin.php
+admin_area/login.html
+admin_area/index.html
+admincp/index.asp
+admincp/login.asp
+admincp/index.html
+webadmin/index.html
+webadmin/admin.html
+webadmin/login.html
+admin/admin_login.html
+admin_login.html
+panel-administracion/login.html
+nsw/admin/login.php
+webadmin/login.php
+admin/admin_login.php
+admin_login.php
+admin_area/admin.html
+pages/admin/admin-login.php
+admin/admin-login.php
+admin-login.php
+bb-admin/index.html
+bb-admin/login.html
+bb-admin/admin.html
+admin/home.html
+pages/admin/admin-login.html
+admin/admin-login.html
+admin-login.html
+admin/adminLogin.html
+adminLogin.html
+home.html
+rcjakar/admin/login.php
+adminarea/index.html
+adminarea/admin.html
+webadmin/index.php
+webadmin/admin.php
+user.html
+modelsearch/login.html
+adminarea/login.html
+panel-administracion/index.html
+panel-administracion/admin.html
+modelsearch/index.html
+modelsearch/admin.html
+admincontrol/login.html
+adm/index.html
+adm.html
+user.php
+panel-administracion/login.php
+wp-login.php
+adminLogin.php
+admin/adminLogin.php
+home.php
+adminarea/index.php
+adminarea/admin.php
+adminarea/login.php
+panel-administracion/index.php
+panel-administracion/admin.php
+modelsearch/index.php
+modelsearch/admin.php
+admincontrol/login.php
+adm/admloginuser.php
+admloginuser.php
+admin2/login.php
+admin2/index.php
+adm/index.php
+adm.php
+affiliate.php
+adm_auth.php
+memberadmin.php
+administratorlogin.php
+admin/admin.asp
+admin_area/admin.asp
+admin_area/login.asp
+admin_area/index.asp
+bb-admin/index.asp
+bb-admin/login.asp
+bb-admin/admin.asp
+pages/admin/admin-login.asp
+admin/admin-login.asp
+admin-login.asp
+user.asp
+webadmin/index.asp
+webadmin/admin.asp
+webadmin/login.asp
+admin/admin_login.asp
+admin_login.asp
+panel-administracion/login.asp
+adminLogin.asp
+admin/adminLogin.asp
+home.asp
+adminarea/index.asp
+adminarea/admin.asp
+adminarea/login.asp
+panel-administracion/index.asp
+panel-administracion/admin.asp
+modelsearch/index.asp
+modelsearch/admin.asp
+admincontrol/login.asp
+adm/admloginuser.asp
+admloginuser.asp
+admin2/login.asp
+admin2/index.asp
+adm/index.asp
+adm.asp
+affiliate.asp
+adm_auth.asp
+memberadmin.asp
+administratorlogin.asp
+siteadmin/login.asp
+siteadmin/index.asp
+ADMIN/
+paneldecontrol/
+login/
+cms/
+admon/
+ADMON/
+administrador/
+ADMIN/login.php
+panelc/
+ADMIN/login.html
+/modulo/
+intranet/
+intranet.php
+administrador/
+backend/
+entrada/
+entrada.php
+puertaoculta.php
+puertaoculta/
+back.php
+menu.php
+editnoticia.php
+editarnoticia.php
+editar_noticia.php
+adm.php
+into.php
+entrar.php
+system/
+system.php
+back/
+logout.php
+js/admin/
+admini/
+admin7/
+administr/
+administ/
+administra/
+into/
+entrar/
+puerta/
+entrada/
+admin.php
+login.htm
+login.html
+login/
+login.php
+adm/
+admin/
+admin/account.html
+admin/login.html
+admin/login.htm
+admin/home.php
+admin/controlpanel.html
+admin/controlpanel.htm
+admin/cp.php
+admin/adminLogin.html
+admin/adminLogin.htm
+admin/admin_login.php
+admin/controlpanel.php
+admin/admin-login.php
+admin-login.php
+admin/account.php
+admin/admin.php
+admin.htm
+admin.html
+adminitem/
+adminitem.php
+adminitems/
+adminitems.php
+administrator/login.php
+administrator.php
+administration/
+administration.php
+adminlogin.php
+admin_area/admin.php
+admin_area/
+admin_area/login.php
+manager/
+manager.php
+letmein/
+letmein.php
+superuser/
+superuser.php
+access/
+access.php
+sysadm/
+sysadm.php
+superman/
+supervisor/
+panel.php
+control/
+control.php
+member/
+member.php
+members/
+members.php
+user/
+user.php
+cp/
+uvpanel/
+manage/
+manage.php
+management/
+management.php
+signin/
+signin.php
+log-in/
+log-in.php
+log_in/
+log_in.php
+sign_in/
+sign_in.php
+sign-in/
+sign-in.php
+users/
+users.php
+accounts/
+accounts.php
+wp-login.php
+bb-admin/login.php
+bb-admin/admin.php
+bb-admin/admin.html
+administrator/account.php
+relogin.htm
+relogin.html
+check.php
+relogin.php
+blog/wp-login.php
+user/admin.php
+users/admin.php
+registration/
+processlogin.php
+checklogin.php
+checkuser.php
+checkadmin.php
+isadmin.php
+authenticate.php
+authentication.php
+auth.php
+authuser.php
+authadmin.php
+cp.php
+modelsearch/login.php
+moderator.php
+moderator/
+controlpanel/
+controlpanel.php
+admincontrol.php
+adminpanel.php
+fileadmin/
+fileadmin.php
+sysadmin.php
+admin1.php
+admin1.html
+admin1.htm
+admin2.php
+admin2.html
+yonetim.php
+yonetim.html
+yonetici.php
+yonetici.html
+phpmyadmin/
+myadmin/
+ur-admin.php
+ur-admin/
+Server.php
+Server/
+wp-admin/
+administr8.php
+administr8/
+webadmin/
+webadmin.php
+administratie/
+admins/
+admins.php
+administrivia/
+Database_Administration/
+useradmin/
+sysadmins/
+admin1/
+system-administration/
+administrators/
+pgadmin/
+directadmin/
+staradmin/
+ServerAdministrator/
+SysAdmin/
+administer/
+LiveUser_Admin/
+sys-admin/
+typo3/
+panel/
+cpanel/
+cpanel_file/
+platz_login/
+rclogin/
+blogindex/
+formslogin/
+autologin/
+support_login/
+meta_login/
+manuallogin/
+simplelogin/
+loginflat/
+utility_login/
+showlogin/
+memlogin/
+login-redirect/
+sub-login/
+wp-login/
+login1/
+dir-login/
+login_db/
+xlogin/
+smblogin/
+customer_login/
+Userlogin/
+login-us/
+acct_login/
+bigadmin/
+project-admins/
+phppgadmin/
+pureadmin/
+sql-admin/
+radmind/
+openvpnadmin/
+wizmysqladmin/
+vadmind/
+ezsqliteadmin/
+hpwebjetadmin/
+newsadmin/
+adminpro/
+Lotus_Domino_Admin/
+bbadmin/
+vmailadmin/
+Indy_admin/
+ccp14admin/
+irc-macadmin/
+banneradmin/
+sshadmin/
+phpldapadmin/
+macadmin/
+administratoraccounts/
+admin4_account/
+admin4_colon/
+radmind-1/
+Super-Admin/
+AdminTools/
+cmsadmin/
+SysAdmin2/
+globes_admin/
+cadmins/
+phpSQLiteAdmin/
+navSiteAdmin/
+server_admin_small/
+logo_sysadmin/
+power_user/
+system_administration/
+ss_vms_admin_sm/
+bb-admin/
+panel-administracion/
+instadmin/
+memberadmin/
+administratorlogin/
+adm.php
+admin_login.php
+panel-administracion/login.php
+pages/admin/admin-login.php
+pages/admin/
+acceso.php
+admincp/login.php
+admincp/
+adminarea/
+admincontrol/
+affiliate.php
+adm_auth.php
+memberadmin.php
+administratorlogin.php
+modules/admin/
+administrators.php
+siteadmin/
+siteadmin.php
+adminsite/
+kpanel/
+vorod/
+vorod.php
+vorud/
+vorud.php
+adminpanel/
+PSUser/
+secure/
+webmaster/
+webmaster.php
+autologin.php
+userlogin.php
+admin_area.php
+cmsadmin.php
+security/
+usr/
+root/
+secret/
+admin/login.php
+admin/adminLogin.php
+moderator.php
+moderator.html
+moderator/login.php
+moderator/admin.php
+yonetici.php
+0admin/
+0manager/
+aadmin/
+cgi-bin/login.php
+login1.php
+login_admin/
+login_admin.php
+login_out/
+login_out.php
+login_user.php
+loginerror/
+loginok/
+loginsave/
+loginsuper/
+loginsuper.php
+login.php
+logout/
+logout.php
+secrets/
+super1/
+super1.php
+super_index.php
+super_login.php
+supermanager.php
+superman.php
+superuser.php
+supervise/
+supervise/Login.php
+super.php";
+function template() {
+    echo '
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta **********="Content-Type" *********"text/html; charset=utf-8" />
+<title>Admin page Finder V2 By R4lph</title>
+<style type="text/css">
+h1.technique-two {
+        
+        background: url("http://www.alboraaq.com/jpg/CpD39032.gif") repeat top center;
+        margin: 0 auto;
+}
+body{
+    background: #000000;
+    margin: 0;
+    padding: 0;
+    padding-top: 10px;
+    color: #FFF;
+    font-family: courrier new;
+    font-size: 15px;
+}
+a{
+    color: #FFF;
+    text-decoration: none;
+    font-weight: bold;
+}
+.wrapper{
+    width: 1000px;
+    margin: 0 auto;
+}
+.tube{
+    padding: 10px;
+}
+.red{
+    width: 998px;
+    border: 1px solid LIME;
+    background: #191919;
+    color: LIME;
+}
+.red input{
+    background: #000;
+    border: 1px solid LIME;
+    color: white;
+}
+.blue{
+    float: left;
+    width: 1000px;
+    border: 1px solid LIME;
+    background: #191919;
+    color: RED;
+}
+.green{
+    float: left;
+    width: 1000px;
+    border: 1px solid LIME;
+    background: #191919;
+    color: LIME;
+}
+</style>
+<script type="text/javascript">
+<!--
+function insertcode($text, $place, $replace)
+{
+    var $this = $text;
+    var logbox = document.getElementById($place);
+    if($replace == 0)
+        document.getElementById($place).innerHTML = logbox.innerHTML+$this;
+    else
+        document.getElementById($place).innerHTML = $this;
+//document.getElementById("helpbox").innerHTML = $this;
+}
+-->
+</script>
+<center><pre>
+   _____       .___      .__       ___________.__            .___          ____   ____________     _______   
+  /  _  \    __| _/_____ |__| ____ \_   _____/|__| ____    __| _/__________\   \ /   /\_____  \    \   _  \  
+ /  /_\  \  / __ |/     \|  |/    \ |    __)  |  |/    \  / __ |/ __ \_  __ \   Y   /  /  ____/    /  /_\  \ 
+/    |    \/ /_/ |  Y Y  \  |   |  \|     \   |  |   |  \/ /_/ \  ___/|  | \/\     /  /       \    \  \_/   \____|__  /\____ |__|_|  /__|___|  /\___  /   |__|___|  /\____ |\___  >__|    \___/   \_______ \ /\ \_____  /
+        \/      \/     \/        \/     \/            \/      \/    \/                        \/ \/       \/ 
+                                            POWER BY R4LPH
+                
+</pre></center>   
+</head>
+<body>
+<h1 class="technique-two">     
+</h1>
+<div class="wrapper">
+<div class="red">
+<div class="tube">
+<form action="" method="post" name="xploit_form">
+Pega aqui la pagina:<br /><input type="text" name="xploit_url" value="' . $_POST['xploit_url'] . '" style="width: 100%;" /><br /><br />
+</form>
+<br />
+</div> <!-- /tube -->
+</div> <!-- /red -->
+<br />
+<div class="green">
+<div class="tube" id="rightcol">
+Paneles escaneados: <span id="verified">0</span> / <span id="total">0</span><br />
+Paneles encontrados:<br />
+</div> <!-- /tube -->
+</div> <!-- /green -->
+<br clear="all" /><br />
+<div class="blue">
+<div class="tube" id="logbox">
+<br />
+<br />
+<center>Admin page Finder v2.0 Power By R4lph -- Greetzz to: Okol<br /><br />
+</div> <!-- /tube -->
+</div> <!-- /blue -->
+</div> <!-- /wrapper -->
+<br clear="all">';
+}
+function show($msg, $br = 1, $stop = 0, $place = 'logbox', $replace = 0) {
+    if ($br == 1) $msg.= "<br />";
+    echo "<script type=\"text/javascript\">insertcode('" . $msg . "', '" . $place . "', '" . $replace . "');</script>";
+    if ($stop == 1) exit;
+    @flush();
+    @ob_flush();
+}
+function check($x, $front = 0) {
+    global $_POST, $site, $false;
+    if ($front == 0) $t = $site . $x;
+    else $t = 'http://' . $x . '.' . $site . '/';
+    $headers = get_headers($t);
+    if (!eregi('200', $headers[0])) return 0;
+    $data = @file_get_contents($t);
+    if ($_POST['xploit_404string'] == "") if ($data == $false) return 0;
+    if ($_POST['xploit_404string'] != "") if (strpos($data, $_POST['xploit_404string'])) return 0;
+    return 1;
+}
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+template();
+if (!isset($_POST['xploit_url'])) die;
+if ($_POST['xploit_url'] == '') die;
+$site = $_POST['xploit_url'];
+if ($site[strlen($site) - 1] != "/") $site.= "/";
+if ($_POST['xploit_404string'] == "") $false = @file_get_contents($site . "d65897f5380a21a42db94b3927b823d56ee1099a-this_can-t_exist.html");
+$list['end'] = str_replace("
+", "", $list['end']);
+$list['front'] = str_replace("
+", "", $list['front']);
+$pathes = explode("
+", $list['end']);
+$frontpathes = explode("
+", $list['front']);
+show(count($pathes) + count($frontpathes), 1, 0, 'total', 1);
+$verificate = 0;
+foreach ($pathes as $path) {
+    show('Analizando ' . $site . $path . ' : ', 0, 0, 'logbox', 0);
+    $verificate++;
+    show($verificate, 0, 0, 'verified', 1);
+    if (check($path) == 0) show('not found', 1, 0, 'logbox', 0);
+    else {
+        show('<span style="color: #00FF00;"><strong>found</strong></span>', 1, 0, 'logbox', 0);
+        show('<a href="' . $site . $path . '">' . $site . $path . '</a>', 1, 0, 'rightcol', 0);
+    }
+}
+preg_match("/\/\/(.*?)\//i", $site, $xx);
+$site = $xx[1];
+if (substr($site, 0, 3) == "www") $site = substr($site, 4);
+foreach ($frontpathes as $frontpath) {
+    show('Analizando http://' . $frontpath . '.' . $site . '/ : ', 0, 0, 'logbox', 0);
+    $verificate++;
+    show($verificate, 0, 0, 'verified', 1);
+    if (check($frontpath, 1) == 0) show('not found', 1, 0, 'logbox', 0);
+    else {
+        show('<span style="color: #00FF00;"><strong>found</strong></span>', 1, 0, 'logbox', 0);
+        show('<a href="http://' . $frontpath . '.' . $site . '/">' . $frontpath . '.' . $site . '</a>', 1, 0, 'rightcol', 0);
+    }
+}
+?>
